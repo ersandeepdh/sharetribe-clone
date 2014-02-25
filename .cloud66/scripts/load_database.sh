a5095dad3713b63fb1cfd2859ed4ef199d66e2e1
Elton Okada <eltonokada@gmail.com>
2014-02-25 10:16:52 -0300

    cloud 66 script

--- .cloud66/scripts/load_database.sh
+++ .cloud66/scripts/load_database.sh
@@ -1,5 +1,5 @@
 ### /.CLOUD66/SCRIPTS/LOAD_DATABASE.SH ###
 
 cd $STACK_PATH
-bundle exec rake db:create RAILS_ENV=production
-bundle exec rake db:schema:load RAILS_ENV=production
+bundle exec rake RAILS_ENV=production db:create 
+bundle exec rake RAILS_ENV=production db:schema:load
