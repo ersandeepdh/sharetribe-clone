ff5af9ea455186997d888dec0ffb5d8ed729d9a3
Elton Okada <eltonokada@gmail.com>
2014-03-17 10:25:20 -0300

    enabling asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Kassi
     config.autoload_paths += Dir[Rails.root.join('app', 'services')]
     
     # Enable the asset pipeline  
-    config.assets.enabled = false  
+    config.assets.enabled = true  
 
     # Version of your assets, change this if you want to expire all your assets  
     config.assets.version = '1.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Kassi::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = APP_CONFIG.serve_static_assets_in_production || false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
