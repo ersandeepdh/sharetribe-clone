7b307fcf0346a11d0073bef71f395b700b47125b
Elton Okada <eltonokada@gmail.com>
2014-03-26 22:28:07 -0300

    asset configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Kassi::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = APP_CONFIG.serve_static_assets_in_production || false
+  #config.serve_static_assets = APP_CONFIG.serve_static_assets_in_production || false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -42,7 +42,7 @@ Kassi::Application.configure do
   config.assets.compress = true  
 
   # Don't fallback to assets pipeline  
-  config.assets.compile = false  
+  config.assets.compile = true  
 
   # Generate digests for assets URLs  
   config.assets.digest = true
