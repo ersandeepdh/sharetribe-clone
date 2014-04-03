b0da859aaf4e6b7c4a770328bd32426e9afe8f03
Elton Okada <eltonokada@gmail.com>
2014-04-03 20:01:01 -0300

    deployment options

--- .cloud66/scripts/rake_tasks.sh
+++ .cloud66/scripts/rake_tasks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # access your Rails stack path
 cd $RAILS_STACK_PATH
-# run your rake task
+
+chown -R nginx:nginx releases/
 bundle exec rake sharetribe:generate_customization_stylesheets RAILS_ENV=production
 bundle exec rake assets:precompile:all RAILS_ENV=production RAILS_GROUPS=assets
