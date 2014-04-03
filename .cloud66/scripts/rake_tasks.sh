373a98d2f9616788c34ae408d518c48ad6ee56b5
Elton Okada <eltonokada@gmail.com>
2014-04-03 19:35:09 -0300

    deployment changes

--- .cloud66/scripts/rake_tasks.sh
+++ .cloud66/scripts/rake_tasks.sh
@@ -3,4 +3,4 @@
 cd $RAILS_STACK_PATH
 # run your rake task
 bundle exec rake sharetribe:generate_customization_stylesheets RAILS_ENV=production
-bundle exec rake assets:precompile RAILS_ENV=production
+bundle exec rake assets:precompile:all RAILS_ENV=production RAILS_GROUPS=assets
