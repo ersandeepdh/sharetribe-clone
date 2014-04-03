837604c5daa1058b20914a7686d2e51fc1511edc
Elton Okada <eltonokada@gmail.com>
2014-04-03 20:24:59 -0300

    testing cloud66 deployment hooks

--- .cloud66/scripts/rake_tasks.sh
+++ .cloud66/scripts/rake_tasks.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # access your Rails stack path
 cd $RAILS_STACK_PATH
-
-chown -R nginx:nginx releases/
 bundle exec rake sharetribe:generate_customization_stylesheets RAILS_ENV=production
 bundle exec rake assets:precompile:all RAILS_ENV=production RAILS_GROUPS=assets
