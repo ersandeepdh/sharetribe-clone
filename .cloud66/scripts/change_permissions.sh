bd638c69d9ea35065ad9b053b4f88ad03da70e58
Elton Okada <eltonokada@gmail.com>
2014-04-22 13:59:26 -0300

    testing cloud66 deployment

--- .cloud66/scripts/change_permissions.sh
+++ .cloud66/scripts/change_permissions.sh
@@ -2,4 +2,4 @@
 #load environment variables
 source /var/.cloud66_env
 #assign desired permissions
-cd $RAILS_STACK_PATH/../ && sudo chmod 777 -R releases/
+cd $RAILS_STACK_PATH/../ && chmod 777 -R releases/
